a62390bf17101161339e43d1abf0608bbcafd050
objetive218 <davidsantiagotc@gmail.com>
2024-06-13 23:53:37 -0500

    cors

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const Single = require("./models/single");
 const { default: helmet } = require("helmet");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(
@@ -21,7 +22,6 @@ app.use(
     }
   )
 );
-app.use(cors());
 connectDb();
 /*
 app.use((req, res, next) => {
